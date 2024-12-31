ad9bc35498793fae29d0d50573f65a433fa45e51
Suvansh <suvanshd27@gmail.com>
2025-01-01 02:50:53 +0530

    Initial commit

---
+++ Count Digits/solution.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the number of digits in n that evenly divide n
+    int evenlyDivides(int n) {
+        // code here
+        int m=n;
+        int cnt=0;
+        
+        while(m>0)
+        {
+            if((m%10)!=0 && n%(m%10)==0)
+            {
+                cnt++;
+            }
+            m=m/10;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
