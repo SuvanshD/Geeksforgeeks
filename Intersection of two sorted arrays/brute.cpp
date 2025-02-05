f109a2e3afdb9e3a1aa040062e60fdf8faaebc3b
Suvansh <suvanshd27@gmail.com>
2025-02-05 16:18:46 +0530

    Runtime Error: 110/115 Test cases

---
+++ Intersection of two sorted arrays/brute.cpp
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+// C++ program to find union of
+// two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the intersection of two arrays.
+    vector<int> intersection(vector<int> &arr1, vector<int> &arr2) {
+        // Your code here
+        
+        // set <int> final;
+        // int i=0;
+        // int j=0;
+        
+        // while (i<arr1.size() and j<arr2.size())
+        // {
+        //     if(arr1[i]<arr2[j])
+        //     {
+        //         i++;
+        //     }
+        //     else if(arr1[i]>arr2[j])
+        //     {
+        //         j++;
+        //     }
+        //     else
+        //     {
+                
+        //         final.insert(arr1[i]);
+        //         i++;
+        //         j++;
+                
+        //     }
+        // }
+    
+        // vector <int> v(final.begin(),final.end());
+        // return v;
+        
+        
+        int n1=arr1.size();
+        int n2=arr2.size();
+        set <int> sets;
+        vector <int> vis (n2,0);
+        
+        for(int i=0;i<n1;i++)
+        {
+            for(int j=0;j<n2;j++)
+            {
+                if(arr1[i]==arr2[j] && vis[j]==0)
+                {
+                    sets.insert(arr1[i]);
+                    vis[j]=1;
+                    break;
+                }
+                else if(arr2[j]>arr1[i]) break;
+            }
+        }
+        
+        vector <int> ans(sets.begin(),sets.end());
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test above function */
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+
+        vector<int> arr1, arr2;
+        string input1;
+        getline(cin, input1);
+        stringstream ss(input1);
+        int number1;
+        while (ss >> number1) {
+            arr1.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream sss(input2);
+        int number2;
+        while (sss >> number2) {
+            arr2.push_back(number2);
+        }
+        Solution ob;
+        // Function calling
+        vector<int> v;
+        v = ob.intersection(arr1, arr2);
+
+        if (v.size() > 0) {
+            for (int i = 0; i < v.size(); i++)
+                cout << v[i] << " ";
+        } else {
+            cout << "[]";
+        }
+
+        cout << endl;
+        cout << "~" << endl;
+        // string tilde;
+        // getline(cin, tilde);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
